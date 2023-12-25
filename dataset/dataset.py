5291fa52065edf4c47c3ce2e2b4a94c484e10cc9
Karl-Liu-ch <liu_ht2010@qq.com>
2023-12-25 10:53:02 +0100

    updaate

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -19,13 +19,45 @@ datanames = ['BGU/','ARAD/']
 # datanames = ['ARAD/', 'ICVL_Norm/']
 # datanames = ['CAVE/', 'ARAD/', 'ICVL_Norm/']
 
-class TrainDataset(Dataset):
+class GetDataset(Dataset):
     def __init__(self, data_root, crop_size, valid_ratio, test_ratio, arg=True):
         self.datanames = datanames
         self.crop_size = crop_size
         self.arg = arg
         self.rgb = []
         self.hyper = []
+
+    def augmentation(self, img, rotTimes, vFlip, hFlip):
+        # Random rotation
+        for j in range(rotTimes):
+            img = np.rot90(img.copy(), axes=(1, 2))
+        # Random vertical Flip
+        for j in range(vFlip):
+            img = img[:, :, ::-1].copy()
+        # Random horizontal Flip
+        for j in range(hFlip):
+            img = img[:, ::-1, :].copy()
+        return img
+
+    def __getitem__(self, idx):
+        bgr = self.rgb[idx]
+        hyper = self.hyper[idx]
+        bgr = np.transpose(bgr, [2, 0, 1])
+        hyper = np.transpose(hyper, [2, 0, 1])
+        rotTimes = random.randint(0, 3)
+        vFlip = random.randint(0, 1)
+        hFlip = random.randint(0, 1)
+        if self.arg:
+            bgr = self.augmentation(bgr, rotTimes, vFlip, hFlip)
+            hyper = self.augmentation(hyper, rotTimes, vFlip, hFlip)
+        return np.ascontiguousarray(bgr), np.ascontiguousarray(hyper)
+
+    def __len__(self):
+        return self.length
+
+class TrainDataset(GetDataset):
+    def __init__(self, data_root, crop_size, valid_ratio, test_ratio, arg=True):
+        super().__init__(data_root, crop_size, valid_ratio, test_ratio, arg=True)
         self.trainset = []
         for name in self.datanames:
             self.trainset.append(split_train(data_root+ 'clean/' + name,valid_ratio=valid_ratio, test_ratio=test_ratio, imsize=self.crop_size))
@@ -34,41 +66,9 @@ class TrainDataset(Dataset):
             self.hyper.extend(trainset[0])
         self.length = len(self.hyper)
 
-    def arguement(self, img, rotTimes, vFlip, hFlip):
-        # Random rotation
-        for j in range(rotTimes):
-            img = np.rot90(img.copy(), axes=(1, 2))
-        # Random vertical Flip
-        for j in range(vFlip):
-            img = img[:, :, ::-1].copy()
-        # Random horizontal Flip
-        for j in range(hFlip):
-            img = img[:, ::-1, :].copy()
-        return img
-
-    def __getitem__(self, idx):
-        bgr = self.rgb[idx]
-        hyper = self.hyper[idx]
-        bgr = np.transpose(bgr, [2, 0, 1])
-        hyper = np.transpose(hyper, [2, 0, 1])
-        rotTimes = random.randint(0, 3)
-        vFlip = random.randint(0, 1)
-        hFlip = random.randint(0, 1)
-        if self.arg:
-            bgr = self.arguement(bgr, rotTimes, vFlip, hFlip)
-            hyper = self.arguement(hyper, rotTimes, vFlip, hFlip)
-        return np.ascontiguousarray(bgr), np.ascontiguousarray(hyper)
-
-    def __len__(self):
-        return self.length
-
-class TestDataset(Dataset):
+class TestDataset(GetDataset):
     def __init__(self, data_root, crop_size, valid_ratio, test_ratio, arg=True):
-        self.datanames = datanames
-        self.crop_size = crop_size
-        self.arg = arg
-        self.rgb = []
-        self.hyper = []
+        super().__init__(data_root, crop_size, valid_ratio, test_ratio, arg=True)
         self.testset = []
         for name in self.datanames:
             self.testset.append(split_test(data_root+ 'clean/' + name, valid_ratio= valid_ratio, test_ratio=test_ratio, imsize=self.crop_size))
@@ -77,76 +77,16 @@ class TestDataset(Dataset):
             self.hyper.extend(testset[0])
         self.length = len(self.hyper)
 
-    def arguement(self, img, rotTimes, vFlip, hFlip):
-        # Random rotation
-        for j in range(rotTimes):
-            img = np.rot90(img.copy(), axes=(1, 2))
-        # Random vertical Flip
-        for j in range(vFlip):
-            img = img[:, :, ::-1].copy()
-        # Random horizontal Flip
-        for j in range(hFlip):
-            img = img[:, ::-1, :].copy()
-        return img
-
-    def __getitem__(self, idx):
-        bgr = self.rgb[idx]
-        hyper = self.hyper[idx]
-        bgr = np.transpose(bgr, [2, 0, 1])
-        hyper = np.transpose(hyper, [2, 0, 1])
-        rotTimes = random.randint(0, 3)
-        vFlip = random.randint(0, 1)
-        hFlip = random.randint(0, 1)
-        if self.arg:
-            bgr = self.arguement(bgr, rotTimes, vFlip, hFlip)
-            hyper = self.arguement(hyper, rotTimes, vFlip, hFlip)
-        return np.ascontiguousarray(bgr), np.ascontiguousarray(hyper)
-
-    def __len__(self):
-        return self.length
-    
-class ValidDataset(Dataset):
+class ValidDataset(GetDataset):
     def __init__(self, data_root, crop_size, valid_ratio, test_ratio, arg=True):
-        self.datanames = datanames
-        self.crop_size = crop_size
-        self.arg = arg
-        self.rgb = []
-        self.hyper = []
-        self.testset = []
+        super().__init__(data_root, crop_size, valid_ratio, test_ratio, arg=True)
+        self.validset = []
         for name in self.datanames:
-            self.testset.append(split_valid(data_root+ 'clean/' + name, valid_ratio= valid_ratio, test_ratio=test_ratio, imsize=self.crop_size))
-        for testset in self.testset:
-            self.rgb.extend(testset[1])
-            self.hyper.extend(testset[0])
+            self.validset.append(split_valid(data_root+ 'clean/' + name, valid_ratio= valid_ratio, test_ratio=test_ratio, imsize=self.crop_size))
+        for validset in self.validset:
+            self.rgb.extend(validset[1])
+            self.hyper.extend(validset[0])
         self.length = len(self.hyper)
-
-    def arguement(self, img, rotTimes, vFlip, hFlip):
-        # Random rotation
-        for j in range(rotTimes):
-            img = np.rot90(img.copy(), axes=(1, 2))
-        # Random vertical Flip
-        for j in range(vFlip):
-            img = img[:, :, ::-1].copy()
-        # Random horizontal Flip
-        for j in range(hFlip):
-            img = img[:, ::-1, :].copy()
-        return img
-
-    def __getitem__(self, idx):
-        bgr = self.rgb[idx]
-        hyper = self.hyper[idx]
-        bgr = np.transpose(bgr, [2, 0, 1])
-        hyper = np.transpose(hyper, [2, 0, 1])
-        rotTimes = random.randint(0, 3)
-        vFlip = random.randint(0, 1)
-        hFlip = random.randint(0, 1)
-        if self.arg:
-            bgr = self.arguement(bgr, rotTimes, vFlip, hFlip)
-            hyper = self.arguement(hyper, rotTimes, vFlip, hFlip)
-        return np.ascontiguousarray(bgr), np.ascontiguousarray(hyper)
-
-    def __len__(self):
-        return self.length
 
 if __name__ == '__main__':
     trainset = TrainDataset(root, 128, 0.1, 0.1)
